01fc4095815671906b4278eb68835da4deac5bd0
sh111-coder <ohk9134@naver.com>
2023-05-13 02:52:31 +0900

    feat: 노선 조회, 목록 조회에 필요한 Dao 구현

--- src/test/java/fixtures/LineFixtures.java
+++ src/test/java/fixtures/LineFixtures.java
@@ -1,15 +1,37 @@
 package fixtures;
 
+import subway.domain.line.Line;
 import subway.dto.LineFindResponse;
 
 import java.util.List;
 
 public class LineFixtures {
 
+    /**
+     * Dummy Data
+     */
     public static final String LINE2_NAME = "2호선";
     public static final String LINE7_NAME = "7호선";
-    public static final LineFindResponse LINE2_노선도 = new LineFindResponse(LINE2_NAME, List.of("잠실역", "강변역", "건대역"));
-    public static final LineFindResponse LINE7_노선도 = new LineFindResponse(LINE7_NAME, List.of("온수역", "대림역", "논현역", "장암역"));
+    public static final long LINE2_ID = 1L;
+    public static final long LINE7_ID = 2L;
 
-    public static final List<LineFindResponse> ALL_노선도 = List.of(LINE2_노선도, LINE7_노선도);
+    /**
+     * Insert Entity
+     */
+    public static final Line LINE2_INSERT_ENTITY = new Line(null, LINE2_NAME);
+    public static final Line LINE7_INSERT_ENTITY = new Line(null, LINE7_NAME);
+
+    /**
+     * Find Entity
+     */
+    public static final Line LINE2_FIND_ENTITY = new Line(LINE2_ID, LINE2_NAME);
+    public static final Line LINE7_FIND_ENTITY = new Line(LINE7_ID, LINE7_NAME);
+
+    /**
+     * Response
+     */
+    public static final LineFindResponse LINE2_FIND_RESPONSE = new LineFindResponse(LINE2_NAME, List.of("잠실역", "강변역", "건대역"));
+    public static final LineFindResponse LINE7_FIND_RESPONSE = new LineFindResponse(LINE7_NAME, List.of("온수역", "대림역", "논현역", "장암역"));
+
+    public static final List<LineFindResponse> ALL_노선도 = List.of(LINE2_FIND_RESPONSE, LINE7_FIND_RESPONSE);
 }
--- src/test/java/subway/dao/LineDaoTest.java
+++ src/test/java/subway/dao/LineDaoTest.java
@@ -1,5 +1,12 @@
 package subway.dao;
 
+import static fixtures.LineFixtures.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,12 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
-import subway.entity.LineEntity;
-
-import java.util.Optional;
-
-import static fixtures.StationFixtures.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import subway.domain.line.Line;
 
 @JdbcTest
 @Sql({"/test-schema.sql", "/test-data.sql"})
@@ -29,28 +31,101 @@ class LineDaoTest {
     }
 
     @Test
-    @DisplayName("Line을 저장한다.")
+    @DisplayName("노선을 저장한다.")
     void insertTest() {
         // given
-        LineEntity insertEntity = LINE7_INSERT_ENTITY;
+        Line insertEntity = LINE7_INSERT_ENTITY;
 
         // when
-        Long insertedLineId = lineDao.insert(insertEntity);
+        Line insertedLine = lineDao.insert(insertEntity);
 
         // then
-        assertThat(insertedLineId).isEqualTo(2L);
+        assertThat(insertedLine)
+                .usingRecursiveComparison()
+                .isEqualTo(LINE7_FIND_ENTITY);
+    }
+
+    @Test
+    @DisplayName("노선 ID에 해당하는 행을 조회한다.")
+    void findByIdTest() {
+        // given
+        long line2Id = LINE2_ID;
+
+        // when
+        Optional<Line> findLine = lineDao.findById(line2Id);
+
+        // then
+        assertThat(findLine.get())
+                .usingRecursiveComparison()
+                .isEqualTo(LINE2_FIND_ENTITY);
+    }
+
+    @Test
+    @DisplayName("노선 ID에 해당하는 행이 없으면 빈 Optional이 반환된다.")
+    void findByIdEmptyOptional() {
+        // given
+        long line7Id = LINE7_ID;
+
+        // when
+        Optional<Line> findNullableLine = lineDao.findById(line7Id);
+
+        // then
+        assertThat(findNullableLine.isEmpty()).isTrue();
     }
 
     @Test
     @DisplayName("노선 이름에 해당하는 행을 조회한다.")
     void findByLineNameTest() {
         // given
-        String lineName = DUMMY_LINE2_NAME;
+        String lineName = LINE2_NAME;
 
         // when
-        Optional<LineEntity> findLine = lineDao.findByLineName(lineName);
+        Optional<Line> findLine = lineDao.findByLineName(lineName);
 
         // then
-        assertThat(findLine.get()).isEqualTo(LINE2_FIND_ENTITY);
+        assertThat(findLine.get())
+                .usingRecursiveComparison()
+                .isEqualTo(LINE2_FIND_ENTITY);
+    }
+
+    @Test
+    @DisplayName("노선 이름에 해당하는 행이 없으면 빈 Optional이 반환된다.")
+    void findByLineNameEmptyOptional() {
+        // given
+        String line7Name = LINE7_NAME;
+
+        // when
+        Optional<Line> findNullableLine = lineDao.findByLineName(line7Name);
+
+        // then
+        assertThat(findNullableLine.isEmpty()).isTrue();
+    }
+
+    @DisplayName("모든 행을 조회한다.")
+    @Test
+    void findAll() {
+        // when
+        List<Line> lines = lineDao.findAll();
+        Line line2 = lines.get(0);
+
+        // then
+        assertAll(
+                () -> assertThat(lines.size()).isEqualTo(1),
+                () -> assertThat(line2).usingRecursiveComparison().isEqualTo(LINE2_FIND_ENTITY)
+        );
+    }
+
+    @DisplayName("노선 ID에 해당하는 행을 삭제한다.")
+    @Test
+    void deleteById() {
+        // given
+        long line2Id = LINE2_ID;
+
+        // when
+        lineDao.deleteById(line2Id);
+        Optional<Line> findNullableLine = lineDao.findById(line2Id);
+
+        // then
+        assertThat(findNullableLine.isEmpty()).isTrue();
     }
 }
